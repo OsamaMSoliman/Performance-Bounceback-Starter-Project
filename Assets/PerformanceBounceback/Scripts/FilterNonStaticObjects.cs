7773a08ad372e1d84be892206a735eda41401825
OsamaNsr <es-osama.nsr1318@alexu.edu.eg>
2019-01-09 10:36:48 +0200

    2nd submission
    1_) changed the directional light to Mixed (and also static)
    2_) selected  8x on the top quality and  4x MSAA option for the 2 below it.
    3_) i made the following static:
     the ScoreBoards / GameManager / BallSpawner
    4_) in the light settings, changed the light mode of the mixed lighting from Subtractive to Shadowmask ,, this made the game lit up

---
+++ Assets/PerformanceBounceback/Scripts/FilterNonStaticObjects.cs
@@ -0,0 +1,57 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class FilterNonStaticObjects : EditorWindow
+{
+    [MenuItem("Custom/Select All Non-Static")]
+    static void FindAllNonStatic()
+    {
+        Object[] gameObjects = FindObjectsOfType(typeof(GameObject));
+
+        GameObject[] gameObjectArray;
+        gameObjectArray = new GameObject[gameObjects.Length];
+
+        int arrayPointer = 0;
+
+        foreach (GameObject gameObject in gameObjects)
+        {
+            StaticEditorFlags flags = GameObjectUtility.GetStaticEditorFlags(gameObject);
+
+            if ((flags & StaticEditorFlags.LightmapStatic) == 0)
+            {
+                gameObjectArray[arrayPointer] = gameObject;
+                arrayPointer += 1;
+            }
+        }
+
+        Selection.objects = gameObjectArray;
+
+    }
+
+
+    [MenuItem("Custom/Select All Static")]
+    static void FindAllStatic()
+    {
+        Object[] gameObjects = FindObjectsOfType(typeof(GameObject));
+
+        GameObject[] gameObjectArray;
+        gameObjectArray = new GameObject[gameObjects.Length];
+
+        int arrayPointer = 0;
+
+        foreach (GameObject gameObject in gameObjects)
+        {
+            StaticEditorFlags flags = GameObjectUtility.GetStaticEditorFlags(gameObject);
+
+            if ((flags & StaticEditorFlags.LightmapStatic) != 0)
+            {
+                gameObjectArray[arrayPointer] = gameObject;
+                arrayPointer += 1;
+            }
+        }
+
+        Selection.objects = gameObjectArray;
+
+    }
+}
